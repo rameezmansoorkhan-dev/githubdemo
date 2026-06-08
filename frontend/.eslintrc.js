190dd82bfeeb33f7b9a49e44bdf084ab6ca7a463
Rameez Khan <rameezmansoorkhan@gmail.com>
2026-06-08 10:39:05 +0000

    build fix

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,18 @@
+// frontend/.eslintrc.js
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    // Add your custom rules here
+  },
+};
